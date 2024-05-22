95e86facd6ee13fdab97acb08873f7caec6738e7
Daniel Eldon <52330149+Eldon-498@users.noreply.github.com>
2024-05-22 22:12:06 +0300

    Initial commit and Project set up

---
+++ src/app/components/schools-details/schools-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schools-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './schools-details.component.html',
+  styleUrl: './schools-details.component.css'
+})
+export class SchoolsDetailsComponent {
+
+}
