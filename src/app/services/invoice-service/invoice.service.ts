010bf0b7d9029560057d43190cfe343c8b0eaa4f
Daniel Eldon <52330149+Eldon-498@users.noreply.github.com>
2024-05-23 23:24:20 +0300

    Move invoice service to its own folder

--- src/app/services/invoice-service/invoice.service.ts
+++ src/app/services/invoice-service/invoice.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Observable} from "rxjs";
-import {environment} from "../../environments/environment";
-import {Invoice} from "../interfaces/invoice";
+import {environment} from "../../../environments/environment";
+import {Invoice} from "../../interfaces/invoice";
 import {HttpClient} from "@angular/common/http";
 
 @Injectable({
