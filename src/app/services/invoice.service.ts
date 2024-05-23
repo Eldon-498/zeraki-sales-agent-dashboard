c12bce2c43b5e6226b2099882088731e3a470774
Daniel Eldon <52330149+Eldon-498@users.noreply.github.com>
2024-05-23 23:08:28 +0300

    create invoice service

--- src/app/services/invoice.service.ts
+++ src/app/services/invoice.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
+import {Invoice} from "../interfaces/invoice";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class InvoiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  private apiUrl = `${environment.baseUrl}invoices`;
+  getInvoices(): Observable<Invoice[]> {
+    return this.http.get<Invoice[]>(this.apiUrl);
+  }
+
+  addInvoice(invoice: Invoice): Observable<Invoice> {
+    return this.http.post<Invoice>(this.apiUrl, invoice);
+  }
+
+  updateInvoice(invoice: Invoice): Observable<Invoice> {
+    return this.http.put<Invoice>(`${this.apiUrl}/${invoice.id}`, invoice);
+  }
+
+  deleteInvoice(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
