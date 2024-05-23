f0588c32c126645b9b679ce5c748d18884b897f2
Daniel Eldon <52330149+Eldon-498@users.noreply.github.com>
2024-05-23 15:27:08 +0300

    Logic for piechart component

--- src/app/shared/pie-chart/pie-chart.component.ts
+++ src/app/shared/pie-chart/pie-chart.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Tooltip, TooltipItem} from "chart.js";
+
 
 @Component({
   selector: 'app-pie-chart',
@@ -8,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrl: './pie-chart.component.css'
 })
 export class PieChartComponent {
+  @Input() data: number[] = [];
+  @Input() label: string = '';
 
+  public pieChartLabels: string[] = ['Target Achieved', 'Target Remaining'];
+
+  public pieChartOptions: any = {
+    responsive: true,
+    tooltips: {
+      enabled: true,
+      mode: 'single',
+      callbacks: {
+        label: (tooltipItem:TooltipItem<any>, data:any) => {
+          const dataset = data.datasets[tooltipItem.datasetIndex];
+          const value = dataset.data[tooltipItem.datasetIndex];
+          return `${value} signups`;
+        }
+      }
+    }
+  };
+
+  public pieChartColors: Array<any> = [
+    {
+      backgroundColor: ['#36A2EB', '#FF6384']
+    }
+  ];
 }
