c2e32f2d78130b7388a7a9c6a6c35c24aaca74f4
Daniel Eldon <52330149+Eldon-498@users.noreply.github.com>
2024-05-23 15:08:16 +0300

    create dashboard header

---
+++ src/app/components/dashboard-header/dashboard-header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import theme from "tailwindcss/defaultTheme";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-dashboard-header',
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './dashboard-header.component.html',
+  styleUrl: './dashboard-header.component.css'
+})
+export class DashboardHeaderComponent {
+
+  protected readonly theme = theme;
+
+}
