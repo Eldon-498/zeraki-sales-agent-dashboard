1de8abb378eb7ee3944ed983436c69941093c6bb
Daniel Eldon <52330149+Eldon-498@users.noreply.github.com>
2024-05-23 16:51:39 +0300

    Ts and Template logic creation

--- src/app/shared/bar-graph/bar-graph.component.ts
+++ src/app/shared/bar-graph/bar-graph.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {ChartData, ChartEvent, ChartOptions, ChartType} from "chart.js";
+import {BaseChartDirective} from "ng2-charts";
 
 @Component({
   selector: 'app-bar-graph',
   standalone: true,
-  imports: [],
+  imports: [
+    BaseChartDirective
+  ],
   templateUrl: './bar-graph.component.html',
   styleUrl: './bar-graph.component.css'
 })
 export class BarGraphComponent {
+  @Input() data: ChartData<'bar'> = {
+    labels: [],
+    datasets: []
+  };
+  @Input() label: string = '';
 
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: (context) => {
+            const label = context.dataset.label || '';
+            const value = context.formattedValue;
+            return `${label}: ${value} signups`;
+          }
+        }
+      }
+    },
+    scales: {
+      x: {
+        stacked: true
+      },
+      y: {
+        stacked: true
+      }
+    }
+  };
+
+  public barChartType: ChartType = 'bar';
+  public barChartColors: Array<any> = [
+    { backgroundColor: ['#36A2EB', '#FF6384', '#FFCE56'] }
+  ];
+
+  public chartClicked(event: { event?: ChartEvent; active?: object[] }): void {
+    console.log('Chart clicked:', event);
+    // Add your logic for handling chart click events here
+  }
 }
