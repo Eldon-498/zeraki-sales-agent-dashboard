f22aff33fbc320d6295a8ae588392fa156f1e1da
Daniel Eldon <52330149+Eldon-498@users.noreply.github.com>
2024-05-24 12:20:33 +0300

    Modal logic and update onsubmit, delete and edit

--- src/app/shared/table-component/table-component.component.ts
+++ src/app/shared/table-component/table-component.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, SimpleChanges} from '@angular/core';
+import {Component, EventEmitter, Input, Output, SimpleChanges} from '@angular/core';
 import {NgForOf} from "@angular/common";
 import {ButtonComponentComponent} from "../button-component/button-component.component";
 import {ColumnDefinition} from "../../interfaces/column-definition";
@@ -16,7 +16,9 @@ import {ColumnDefinition} from "../../interfaces/column-definition";
 export class TableComponentComponent {
   @Input() columns: ColumnDefinition[] = [];
   @Input() data: any[] | null = [];
-
+  @Output() editClicked = new EventEmitter<any>();
+  @Output() createClicked = new EventEmitter<any>();
+  @Output() deleteClicked = new EventEmitter<any>();
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['data']) {
       console.log('Table data input:', this.data); // Log the data input to confirm receipt
@@ -24,14 +26,14 @@ export class TableComponentComponent {
   }
 
   onEdit(item: any) {
-
+    this.editClicked.emit(item);
   }
 
   onCreate(item: any) {
-
+  this.createClicked.emit(item);
   }
 
   onDelete(item: any) {
-
+    this.deleteClicked.emit(item);
   }
 }
